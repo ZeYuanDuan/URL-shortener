24d14988f4df91975c026ac853047367de910355
ZeYuanDuan <zyd.zeyuanduan@gmail.com>
2024-03-29 16:16:10 +0800

    style(/views): introduce style enhancements by incorporating a new button
    n

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,19 +2,34 @@ document.querySelector("#url-form").onsubmit = function (event) {
   event.preventDefault();
 
   let url = document.querySelector("#url-input").value;
-  // Placeholder for URL shortening logic
   let shortenedUrl =
     "https://tinyurl.herokuapp.com/" +
-    Math.random().toString(36).substring(2, 8);
+    Math.random().toString(36).substring(2, 7);
 
   document.querySelector("#form-container").style.display = "none";
   document.querySelector("#result-container").style.display = "block";
-  document.querySelector("#shortened-url").value = shortenedUrl;
+  document.querySelector("#shortened-url").innerText = shortenedUrl;
 };
 
+document
+  .querySelector("#copy-button")
+  .addEventListener("click", copyToClipboard);
+
 function copyToClipboard() {
-  let copyText = document.querySelector("#shortened-url");
-  copyText.select();
-  navigator.clipboard.writeText();
-  alert("Copied to clipboard: " + copyText.value);
+  let shortenedUrl = document.querySelector("#shortened-url").innerText;
+  navigator.clipboard.writeText(shortenedUrl).then(
+    function () {
+      alert("Copied to clipboard: " + shortenedUrl);
+    },
+    function (err) {
+      alert("Error in copying text: " + err);
+    }
+  );
+}
+
+document.querySelector("#refresh-button").addEventListener("click", resetForm);
+function resetForm() {
+  document.querySelector("#result-container").style.display = "none";
+  document.querySelector("#url-input").value = "";
+  document.querySelector("#form-container").style.display = "block";
 }
